96f930d0a41380d309b914dabce13c3f532fbff0
Caleb Appleby <s171500@students.aie.edu.au>
2021-05-30 21:51:26 +0930

    Started on commentators
    -bot now stores its messages to be edited
    -fixed a bug where it would pick up rows before they are complete
    -added checking for commentators (needs to reference the correct one still)
    -created new functions and reformatted main

--- atomiccalebbot.py
+++ atomiccalebbot.py
@@ -1,5 +1,5 @@
 # bot.py
-##################VARIABLES#######################################
+##################IMPORTS#######################################
 from __future__ import print_function
 from dotenv import load_dotenv
 
@@ -17,6 +17,8 @@ import os.path
 import discord
 import time
 
+
+##########################VARIABLES#################
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
@@ -25,16 +27,23 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 class Race:
     data = ''
     raceTime = time.gmtime
-    messageID = 0
+    messageData = ''
+    row = 0
+    commentators = []
 
 sharcordRaces = []
 chessRaces = []
 
+#text files
 sharcordTxtFile = 'sharcordRaces.txt'
+sharcordTestTxtFile = 'testSharcordRaces.txt'
 chessTxtFile = 'chessRaces.txt'
+chessTestTxtFile = 'testChessRaces.txt'
+
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_RANGE_NAME = 'A2:E'
+raceDataSampleRange = 'A2:E'
+commentatorsSampleRange = 'F2:G'
 
 #channels
 sharcordChannel = 847495184660955146
@@ -59,11 +68,9 @@ def WriteRacesToFile(races, fileName):
         for race in races:
             f.write(race.data)
 
-def GetRaceData(sheet, hasCategory):
+def GetSheet(sheetToUse, sampleRange):
     """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    sheetToUse = sheet
+    Prints values from a sample spreadsheet."""
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -87,82 +94,146 @@ def GetRaceData(sheet, hasCategory):
     # Call the Sheets API
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=sheetToUse,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+                                range=sampleRange).execute()
+    return result.get('values', [])
+
+def GetRaceData(sheet, hasCategory):
+    values = GetSheet(sheet, raceDataSampleRange)
     raceDatas = []
     if not values:
         print('No data found.')
     else:
+        #get where all the data 'should' be
         dateTime = 0
         category = 1
         runnerOne = 2
         runnerTwo = 4
+        rowsNeeded = 4
+        #then fix it
         if not hasCategory:
             runnerOne -= 1
             runnerTwo -= 1
+            rowsNeeded -= 1
 
         for row in values:
         #if there isnt enoughdata dont bother
             raceData = Race()
-            if len(row) < 3:
+            if len(row) < rowsNeeded:
                 continue
-            if len(row) < 4:
+            #update it again
+            if len(row) < rowsNeeded - 1:
                 runnerTwo -= 1
 
+            #add the data based on hasCategory
             if hasCategory:
                 if row[dateTime] and row[category] and row[runnerOne] and row[runnerTwo]:
                     data = row[dateTime] + ',' + row[category] + ',' + row[runnerOne] +',' + row[runnerTwo] + '\n'
                     raceData.data = data
+                    raceData.row = values.index(row)
             else:
                 if row[dateTime] and row[category] and row[runnerOne] and row[runnerTwo]:
                     data = row[dateTime] +',' + row[runnerOne] +',' + row[runnerTwo] + '\n'
                     raceData.data = data
+                    raceData.row = values.index(row)
 
             raceDatas.append(raceData)
     return raceDatas
 
 def GetMessageString(raceInfo, added, hasCategory, chess):
      raceString = ''
+     #split up the data 
      raceInfoList = raceInfo.split(',')
 
+     #format it correctly
      headerPart = '**New Race Scheduled**' if added else '**Race Removed**'
      raceString += headerPart 
-     raceString += '\n Date/Time:' + raceInfoList[0]
+     raceString += '\n Date/Time: ' + raceInfoList[0]
      if hasCategory:
-        raceString += '\n Category: %s \n Racers: %s VS %s\n' % (raceInfoList[1], raceInfoList[2], raceInfoList[3])
+        raceString += '\n Category: %s \n Racers: %s VS %s Commentators: \n Restreamer: \n' % (raceInfoList[1], raceInfoList[2], raceInfoList[3])
      elif not chess:
-        raceString += '\n Category: ASM \n Racers: %s VS %s\n' % (raceInfoList[1], raceInfoList[2])
+        raceString += '\n Tournament Race \n Racers: %s VS %s Commentators: \n Restreamer: \n' % (raceInfoList[1], raceInfoList[2])
      else:
-        raceString += '\n Racers: %s VS %s\n' % (raceInfoList[1], raceInfoList[2])
+        raceString += '\n Racers: %s VS %s Commentators: \n Restreamer: \n' % (raceInfoList[1], raceInfoList[2])
          
+    #return the formatted string for use
      return raceString
 
 
 def CompareRaces(races, txtFile, hasCategory, chess):
+    #get both datas
     with open(txtFile) as f:
         fileRaces = f.readlines()
     raceDataStrings = []
+    #get the info data
     for race in races:
         raceDataStrings.append(race.data)
 
     i = 0
-    NEWRACES = ''
+    NEWRACES = {}
    
+    #check the race data agaisnt the file
     for race in races:
         if len(race.data) == 0:
             continue
-        found = race.data in fileRaces
-        if not found:
-            NEWRACES += GetMessageString(race.data, True, hasCategory, chess)
+        if not race.data in fileRaces:
+            #if we cant find it a race has been added
+            NEWRACES[race] = GetMessageString(race.data, True, hasCategory, chess)
          
+    #check the file data agaisnt race data
     for fileRace in fileRaces:
-        found = fileRace in raceDataStrings
-        if not found:
-            NEWRACES += GetMessageString(fileRace, False, hasCategory, chess)
-    if len(NEWRACES) == 0:
-        NEWRACES = ''
-        #NEWRACES = 'No New Races'
+        if not fileRace in raceDataStrings:
+            #if we cant find it a race has been removed
+            NEWRACES[race] = GetMessageString(fileRace, False, hasCategory, chess)
     return NEWRACES
+
+async def UpdateCommentators(race, newCommentators):
+    print('updating commentators')
+    messageContent = ''
+    if race.messageData:
+        messageContent = race.messageData.content
+    
+    newCommentatorsString = ''
+    for commentator in newCommentators:
+        newCommentatorsString += commentator + ' ' 
+
+    contentLines = messageContent.split('\n')
+    if len(contentLines) > 4:
+        contentLines[4] = 'Commentators: ' + newCommentatorsString
+
+    newMessage = ''
+    for contentLine in contentLines:
+        newMessage += contentLine + '\n'
+    
+    if race.messageData:
+        await race.messageData.edit(content = newMessage)
+    
+    race.commentators = newCommentators 
+        
+
+async def CheckCommentators(races, sheet, hasCategory):
+    values = GetSheet(sheet, commentatorsSampleRange)
+    newCommentators = []
+    for race in races:
+        if not race.messageData:
+            continue
+        newCommentators.clear()
+        print(race.row)
+        print(len(values))
+        if race.row < len(values):
+            column = 1 if hasCategory else 0
+            if(values[race.row ]):
+                commentatorsRow = values[race.row]
+                if commentatorsRow[column]:
+                    commentators = commentatorsRow[column]
+                    commentatorsSplit = commentators.split('/')
+                    for split in commentatorsSplit:
+                        newCommentators.append(split)
+        if len(newCommentators) < 1:
+            continue
+        print(race.commentators)
+        print(newCommentators)
+        if(len(race.commentators) < 1 or race.commentators[0] != newCommentators[0] or race.commentators[1] != newCommentators[1]):
+            await UpdateCommentators(race, newCommentators)
 
 ########################ACTUAL CODE##############################
 
@@ -172,34 +243,45 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 #client.run(TOKEN)
 
+
 async def CheckRaces(sheet, txtFile, channel, hasCategory, chess):
-    changes = CompareRaces(races, txtFile, hasCategory, chess)
     races = GetRaceData(sheet, hasCategory)
+    changes = CompareRaces(races, txtFile, hasCategory, chess)
     WriteRacesToFile(races, txtFile)
-    if len(changes) != 0:
-        for chunk in Chunks(changes, 2000):
+
+    empty = True
+    #go through every change
+    for change in changes:
+        for chunk in Chunks(changes[change], 2000):
+            #print the change
             print(chunk)
-            await client.get_channel(channel).send(chunk)
-    else:
+            #update the message ID
+            change.messageData = await client.get_channel(channel).send(chunk)
+            empty = False
+    #if its empty print for debug
+    if empty:
         print('no new races for ' + txtFile)
+
+    await CheckCommentators(races, sheet, hasCategory)
     return races
 
 async def CheckSharcordRaces():
-    sharcordRaces = CheckRaces(sharcordSheet, sharcordTxtFile, sharcordChannel, True, False)
+    return await CheckRaces(sharcordSheet, sharcordTxtFile, sharcordChannel, True, False)
 
 async def CheckTestSharcordRaces():
-    sharcordRaces = CheckRaces(testSheet, sharcordTxtFile, testChannel, True, False)
+    return await CheckRaces(testSheet, sharcordTxtFile, testChannel, True, False)
 
 async def CheckChessRaces():
-    chessRaces = CheckRaces(chessSheet, chessTxtFile, chessChannel, False, True)
+    return await CheckRaces(chessSheet, chessTxtFile, chessChannel, False, True)
 
 async def CheckTestChessRaces():
-    chessRaces = CheckRaces(chessSheet, chessTxtFile, testChannelTwo, False, True)
+    return await CheckRaces(chessSheet, chessTxtFile, testChannelTwo, False, True)
+
 
 async def Main():
     while(True):
-        await CheckTestSharcordRaces()
-        await CheckTestChessRaces()
+        sharcordRaces = await CheckTestSharcordRaces()
+        chessRaces = await CheckTestChessRaces()
         time.sleep(15)
 
 
@@ -217,9 +299,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
        return
-    print(message.author + ':' + message.content);
-    if message.content.startswith('races'):
-        for chunk in Chunks(RACEDATA, 2000):
+    #print(message.author + ':' + message.content);
+    
+    if message.content.startswith('sharcord races'):
+        for chunk in Chunks(sharcordRaces, 2000):
+            await message.channel.send(chunk)
+
+    if message.content.startswith('chess races'):
+        for chunk in Chunks(chessRaces, 2000):
             await message.channel.send(chunk)
 
 client.run(TOKEN)
